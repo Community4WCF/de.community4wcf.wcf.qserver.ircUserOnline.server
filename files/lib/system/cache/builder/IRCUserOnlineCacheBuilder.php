34e64a052a801c45697a890275ee7896abb50f47
NurPech <nurpech@nurpech.de>
2013-04-13 17:02:25 +0200

    Signed-off-by: NurPech <nurpech@nurpech.de>

--- files/lib/system/cache/builder/IRCUserOnlineCacheBuilder.php
+++ files/lib/system/cache/builder/IRCUserOnlineCacheBuilder.php
@@ -4,23 +4,23 @@ namespace wcf\system\cache\builder;
 use wcf\util\OpServUtil;
 
 class IRCUserOnlineCacheBuilder extends AbstractCacheBuilder {
-	/**
-	 * @see	wcf\system\cache\builder\AbstractCacheBuilder::rebuild()
-	 */
-	public function rebuild(array $parameters) {
-		$data = array(
-				'channel' => array()
-		);
-		
-		$objectList = new \wcf\data\irc\useronline\channel\ChannelUserOnlineList();
-		$objectList->readObjects();
-		
-		foreach($objectList AS $object) {
-			$data['channel'][$object->channel] = array('channelID' => $object->channelID);
-			$channelData = OpServUtil::getUser(array('name' => $object->channel));
-			$data['channel'][$object->channel]['user'] => $channelData['user'];
-		}
-		
-		return $data;
-	}
+    /**
+     * @see	wcf\system\cache\builder\AbstractCacheBuilder::rebuild()
+     */
+    public function rebuild(array $parameters) {
+        $data = array(
+                'channel' => array()
+        );
+
+        $objectList = new \wcf\data\irc\useronline\channel\ChannelUserOnlineList();
+        $objectList->readObjects();
+
+        foreach($objectList AS $object) {
+            $data['channel'][$object->channel] = array('channelID' => $object->channelID);
+            $channelData = OpServUtil::getUser(array('name' => $object->channel));
+            $data['channel'][$object->channel]['user'] = $channelData['user'];
+        }
+
+        return $data;
+    }
 }
