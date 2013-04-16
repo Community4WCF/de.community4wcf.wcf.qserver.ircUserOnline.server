6d5543b9708c2086859f1af1f935b8bf56951b97
NurPech <nurpech@nurpech.de>
2013-04-16 18:43:18 +0200

    Signed-off-by: NurPech <nurpech@nurpech.de>

--- files/lib/page/IRCUserOnlineDataPage.class.php
+++ files/lib/page/IRCUserOnlineDataPage.class.php
@@ -1,18 +1,18 @@
 <?php
 namespace wcf\page;
 
-class IRCUserOnlineDataPage extens AbstractPage {
-	public $channel = '';
-	public $securityToken = '';
-	public $cache = null;
-	
-	/**
-	 * @see	wcf\page\Page::readParameters()
-	 */
-	public function readParameters() {
-		parent::readParameters();
-		
-		if(isset($_GET['channel'])) $this->channel = escapeString($_GET['channel']);
-		if(isset($_GET['securityToken'])) $this->securityToken = escapeString($_GET['securityToken']);
-	}
+class IRCUserOnlineDataPage extends AbstractPage {
+    public $channel = '';
+    public $securityToken = '';
+    public $cache = null;
+
+    /**
+     * @see	wcf\page\Page::readParameters()
+     */
+    public function readParameters() {
+        parent::readParameters();
+
+        if(isset($_GET['channel'])) $this->channel = escapeString($_GET['channel']);
+        if(isset($_GET['securityToken'])) $this->securityToken = escapeString($_GET['securityToken']);
+    }
 }
